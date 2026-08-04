93bedd77d3b0d58af5bbe56959de114536a72e5e
ANKIT <ankit.tikna92@gmail.com>
2026-08-04 19:11:59 +0530

    Time: 4 ms (28.45%), Space: 19.4 MB (76.76%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -3,9 +3,7 @@ class Solution:
         nums3=nums1+nums2
         nums3.sort()
         median=0
-        m, n = len(nums1), len(nums2)
-
-        mid=(m+n)
+        mid=(len(nums1)+ len(nums2))
         if mid%2==0:
             median=(nums3[int(mid/2)]+nums3[int(mid/2)-1])/2
         else: 
